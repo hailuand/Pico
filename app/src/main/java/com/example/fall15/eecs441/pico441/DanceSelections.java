e4ed01dcc40ebb7252ec5fac3178aa8530b79451
Andreas Hailu <hailuand@umich.edu>
2015-11-11 01:13:31 -0500

    filled ListView with mvp dances

--- app/src/main/java/com/example/fall15/eecs441/pico441/DanceSelections.java
+++ app/src/main/java/com/example/fall15/eecs441/pico441/DanceSelections.java
@@ -2,7 +2,11 @@ package com.example.fall15.eecs441.pico441;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class DanceSelections extends AppCompatActivity {
 
@@ -12,6 +16,37 @@ public class DanceSelections extends AppCompatActivity {
         setContentView(R.layout.activity_dance_selections);
 
         // Get list view
-        ListView danceList = (ListView) findViewById(R.id.dance_list);
+        final ListView danceList = (ListView) findViewById(R.id.dance_list);
+        // Dances to have in ListView
+        String[] dances = new String[] {"Chacha", "Rumba", "Waltz", "Tango", "Twerk"};
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> danceAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, dances);
+        // Assign danceAdapter to listView
+        danceList.setAdapter(danceAdapter);
+        // Add list on click listeners
+        danceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) danceList.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
     }
 }
