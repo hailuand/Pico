0cd7a0ebc4e96251815cbba58f7e96e3ec98c364
Andreas Hailu <hailuand@umich.edu>
2015-11-10 23:21:04 -0500

    android tut complete

--- app/src/main/java/com/example/fall15/eecs441/pico441/MainActivity.java
+++ app/src/main/java/com/example/fall15/eecs441/pico441/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.fall15.eecs441.pico441;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    static public String kEXTRA_MESSAGE = "com.example.fall15.eecs441.pico441.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(kEXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
