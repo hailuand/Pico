f8ca1c7925dabb7a113e4d131f88aea53e94cde3
Andreas Hailu <hailuand@umich.edu>
2015-11-11 10:42:06 -0500

    starting chacha types

---
+++ app/src/main/java/com/example/fall15/eecs441/pico441/ChachaTypes.java
@@ -0,0 +1,14 @@
+package com.example.fall15.eecs441.pico441;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChachaTypes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chacha_types);
+    }
+
+}
