4e2be36aa608841725703cb2324a74b8698608af
Arthur Ozga <aozgaa@umich.edu>
2015-12-10 19:22:59 -0500

    Revert "some dumb changes"
    This reverts commit 9893a8e2d01bf24411d2b775d4438e813ce95db7.

--- app/src/main/java/com/example/fall15/eecs441/pico441/DanceChacha.java
+++ app/src/main/java/com/example/fall15/eecs441/pico441/DanceChacha.java
@@ -1,10 +1,6 @@
 package com.example.fall15.eecs441.pico441;
 
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.graphics.drawable.shapes.Shape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,58 +30,31 @@ public class DanceChacha extends AppCompatActivity {
         AnimationDrawable anim = new AnimationDrawable();
 
         for(FrameInfo frame : steps) {
-            anim.addFrame(generateDrawable(frame), (int) (ms_to_sec * 60 / bpm * frame.duration));
+            anim.addFrame(frame.generateDrawable(), (int) (ms_to_sec * 60 / bpm * frame.duration));
         }
 
         return anim;
     }
 
-//    protected AnimationDrawable getDelayAnimation(AnimationDrawable old, int new_bpm){
-//        AnimationDrawable output = new AnimationDrawable();
-//
-//        for(int i = 0 ; i < old.getNumberOfFrames() ; i++)
-//        {
-//            int old_duration = old.getDuration(i);
-//            output.addFrame(old.getFrame(i), ms_to_sec * 60 / new_bpm);
-//        }
-//        output.setOneShot(false);
-//        return output;
-//    }
-
-    Drawable generateDrawable(FrameInfo f) {
-
-        // TODO: make foot instead of oval
-        // TODO: move foot around the frame based on the coords
-        // TODO: rotate foot based on angle
-
-        // TODO: make a drawable with two shapes (ie: feet).
-
-        // TODO: do something with the duration.
-
-        // TODO: when we actually run this, no shape is visible. Why?
-        // TODO: Also, the button disappears. Did it get moved off the screen?
-
-//        Shape s = new OvalShape();
-//        ShapeDrawable sd = new ShapeDrawable(s);
-//
-//        sd.setIntrinsicWidth(left.x * 100);
-//        return sd;
-
-        ImageView stepView = (ImageView) findViewById(R.id.arrow_img_view);
-        AnimationDrawable oldAnim = (AnimationDrawable) stepView.getDrawable();
-        Drawable frame = oldAnim.getFrame(0);
-
-        return frame;
+    protected AnimationDrawable getDelayAnimation(AnimationDrawable old, int new_bpm){
+        AnimationDrawable output = new AnimationDrawable();
+        for(int i = 0 ; i < old.getNumberOfFrames() ; i++)
+        {
+            int old_duration = old.getDuration(i);
+            output.addFrame(old.getFrame(i), ms_to_sec * 60 / new_bpm);
+        }
+        output.setOneShot(false);
+        return output;
     }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dance_chacha);
 
-        ImageView stepView = (ImageView) findViewById(R.id.arrow_img_view);
-        stepView.setBackgroundResource(R.drawable.arrow_anim_file);
+        ImageView arrow_img = (ImageView) findViewById(R.id.arrow_img_view);
+        arrow_img.setBackgroundResource(R.drawable.arrow_anim_file);
 
-        final AnimationDrawable frameAnimation = (AnimationDrawable) stepView.getBackground();
+        final AnimationDrawable frameAnimation = (AnimationDrawable) arrow_img.getBackground();
 
         frameAnimation.start();
 
@@ -99,11 +68,15 @@ public class DanceChacha extends AppCompatActivity {
                 currentAnimation.stop();
 
                 final AnimationDrawable newAnimation = generateStepsAnimation(DanceSteps, default_bpm);
+
                 arrow_img.setBackground(newAnimation);
-//                stepView.setImageDrawable(newAnimation); // TODO: is this bad?
+
+                // arrow_img.setImageDrawable(newAnimation);
 
                 newAnimation.start();
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/fall15/eecs441/pico441/FrameInfo.java
+++ app/src/main/java/com/example/fall15/eecs441/pico441/FrameInfo.java
@@ -1,12 +1,9 @@
 package com.example.fall15.eecs441.pico441;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.Shape;
-import android.view.View;
-import android.widget.ImageView;
 
 /**
  * Created by aozga on 15/11/24.
@@ -25,4 +22,25 @@ public class FrameInfo {
         duration = duration_;
     }
 
+    Drawable generateDrawable() {
+
+        // TODO: make foot instead of oval
+        // TODO: move foot around the frame based on the coords
+        // TODO: rotate foot based on angle
+
+        // TODO: make a drawable with two shapes (ie: feet).
+
+        // TODO: do something with the duration.
+
+        Shape s = new OvalShape();
+
+        // TODO: when we actually run this, no shape is visible. Why?
+        // TODO: Also, the button disappears. Did it get moved off the screen?
+
+        ShapeDrawable sd = new ShapeDrawable(s);
+
+        // sd.setIntrinsicWidth(left.x * 100);
+
+        return sd;
+    }
 }
